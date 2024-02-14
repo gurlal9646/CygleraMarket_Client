f8db5bfe7270afaa66209d9daa4dda5b21dc0f0e
Gurlal Singh <gurlal.singh@paygdigitals.com>
2024-02-13 20:39:08 -0500

    interceptor changes

--- src/app/guards/auth.http.ts
+++ src/app/guards/auth.http.ts
@@ -3,10 +3,17 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { UserModel } from '../models/user.model';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  constructor(private router: Router) {}
+  user:UserModel;
+  constructor(private router: Router,
+    private authService:AuthService) {
+    this.user = this.authService.getcurrentUserValue();
+
+  }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // Check if the request contains a No-Auth header
@@ -15,17 +22,14 @@ export class TokenInterceptor implements HttpInterceptor {
     }
     
 
-    // Check if token is available in local storage
-    const token = localStorage.getItem('token');
-
-    if (!token) {
+    if (!this.user.token) {
       this.router.navigate(['/error/401']); // Redirect to error page if token is not available
     }
 
     // Attach the token to the request
     request = request.clone({
       setHeaders: {
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${this.user.token}`
       }
     });
 
