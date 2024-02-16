0cbae2d5f34ec259c9634c1c56aab846c4ecc8df
Gurlal Singh <gurlal.singh@paygdigitals.com>
2024-02-16 13:23:16 -0500

    Token expiry validation added

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { UserModel } from '../models/user.model';
+import jwt from 'jsonwebtoken';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard  {
-  user:UserModel;
-  constructor(private router: Router,private authService:AuthService) {
-      this.user = this.authService.getcurrentUserValue();
-    
+export class AuthGuard {
+  user: UserModel;
+  constructor(private router: Router, private authService: AuthService) {
+    this.user = this.authService.getcurrentUserValue();
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    if (this.user.token != '') {
-      return true;
+    if (this.user.token !== '') {
+      // Decode the token
+      const token = this.user.token;
+      const decodedToken: any = jwt.decode(token);
+      if (
+        decodedToken &&
+        decodedToken.iat &&
+        decodedToken.iat > Date.now() / 1000
+      ) {
+        // Token is not expired
+        return true;
+      }
     }
     this.router.navigate(['/error/401']); // Redirect to error page if token is not available
     return false;
--- src/app/guards/auth.http.ts
+++ src/app/guards/auth.http.ts
@@ -1,38 +1,53 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpErrorResponse,
+} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { UserModel } from '../models/user.model';
 import { AuthService } from '../services/auth.service';
+import jwt from 'jsonwebtoken';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  user:UserModel;
-  constructor(private router: Router,
-    private authService:AuthService) {
+  user: UserModel;
+  constructor(private router: Router, private authService: AuthService) {}
 
-  }
-
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     this.user = this.authService.getcurrentUserValue();
 
     // Check if the request contains a No-Auth header
     if (request.headers.has('No-Auth')) {
       return next.handle(request); // Pass the request without modification
     }
-    
 
-    if (!this.user.token) {
-      this.router.navigate(['/error/401']); // Redirect to error page if token is not available
-    }
-
-    // Attach the token to the request
-    request = request.clone({
-      setHeaders: {
-        Authorization: `Bearer ${this.user.token}`
+    if (this.user.token !== '') {
+      // Decode the token
+      const token = this.user.token;
+      const decodedToken: any = jwt.decode(token);
+      if (
+        decodedToken &&
+        decodedToken.iat &&
+        decodedToken.iat > Date.now() / 1000
+      ) {
+        // Attach the token to the request
+        request = request.clone({
+          setHeaders: {
+            Authorization: `Bearer ${this.user.token}`,
+          },
+        });
+      } else {
+        this.router.navigate(['/error/401']); // Redirect to error page if token is not available
       }
-    });
+    }
 
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
